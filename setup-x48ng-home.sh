d5d7f22240ee186b0bedde2c32837868a63f6c46
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2023-04-30 19:08:53 +0200

    better first-run script

--- setup-x48ng-home.sh
+++ setup-x48ng-home.sh
@@ -1,11 +1,15 @@
-mkdir -p ~/.x48ng
+#!/usr/bin/env sh
 
-cp @PREFIX@/share/x48ng/ROMs/gxrom-r ~/.x48ng/rom.dump
-cp ~/.x48ng/rom.dump ~/.x48ng/rom
+TARGET=${TARGET:-.x48ng}
 
-cd ~/.x48ng
+mkdir -p ~/${TARGET}
+
+cp @PREFIX@/share/x48ng/ROMs/gxrom-r ~/${TARGET}/rom.dump
+cp ~/${TARGET}/rom.dump ~/${TARGET}/rom
+
+cd ~/${TARGET}
 
 @PREFIX@/share/x48ng/mkcard 128K port1
 @PREFIX@/share/x48ng/mkcard 4M port2
 
-@PREFIX@/bin/x48ng -initialize
+@PREFIX@/bin/x48ng -initialize -home ${TARGET}
