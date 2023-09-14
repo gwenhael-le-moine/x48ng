b595a19af6c78deb192a9b2a18a3e178e34890ff
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2023-09-14 12:52:58 +0200

    refactor fatal_exit()

--- src/error_handling.c
+++ src/error_handling.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 
 void fatal_exit( char* error, char* advice ) {
-    if ( error[ 0 ] == '\0' ) {
-        fprintf( stderr, "FATAL ERROR, exit.\n" );
-        exit( 1 );
-    }
+    fprintf( stderr, "FATAL ERROR, exit.\n" );
 
-    fprintf( stderr, "FATAL ERROR, exit.\n  - %s\n", error );
+    if ( error[ 0 ] != '\0' )
+        fprintf( stderr, "  - %s\n", error );
 
     if ( advice[ 0 ] != '\0' )
         fprintf( stderr, "  - %s\n", advice );
