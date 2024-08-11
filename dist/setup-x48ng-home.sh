24f52003227077dedfa4cab73a178dc441ef4e26
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2024-08-11 13:51:35 +0200

    don't repeat ~/

--- dist/setup-x48ng-home.sh
+++ dist/setup-x48ng-home.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env sh
 
-DOTX48NG=${DOTX48NG:-.config/x48ng}
-CONFIG_FILE=~/"${DOTX48NG}"/config.lua
+DOTX48NG=${DOTX48NG:-~/.config/x48ng}
+CONFIG_FILE="${DOTX48NG}"/config.lua
 ROM=${ROM:-./ROMs/gxrom-r}
 
-# [ -d ~/"${DOTX48NG}" ] && rm -fr ~"/${DOTX48NG}"
-mkdir -p ~/"${DOTX48NG}"
+mkdir -p "${DOTX48NG}"
 
 [ -e "${CONFIG_FILE}" ] && mv "${CONFIG_FILE}" "${CONFIG_FILE}".orig
 x48ng --print-config > "${CONFIG_FILE}"
 
-cp -r @PREFIX@/share/x48ng/ROMs/ ~/"${DOTX48NG}"/
+cp -r @PREFIX@/share/x48ng/ROMs/ "${DOTX48NG}"/
 
-cd ~/"${DOTX48NG}"/ROMs/ || exit 1
+cd "${DOTX48NG}"/ROMs/ || exit 1
 make get-roms
 
-cd ~/"${DOTX48NG}" || exit 1
+cd "${DOTX48NG}" || exit 1
 [ -e rom ] && mv rom rom.orig
 ln -s "$ROM" rom
 
