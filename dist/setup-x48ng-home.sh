6271bc7daffcf56214beb6a534637aa33fcf84b3
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2024-08-11 13:40:32 +0200

    remove actual ROMs from repo, rework setup script accordingly

--- dist/setup-x48ng-home.sh
+++ dist/setup-x48ng-home.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env sh
 
 DOTX48NG=${DOTX48NG:-.config/x48ng}
+CONFIG_FILE=~/"${DOTX48NG}"/config.lua
+ROM=${ROM:-./ROMs/gxrom-r}
 
-ROM=${ROM:-gxrom-r}
+# [ -d ~/"${DOTX48NG}" ] && rm -fr ~"/${DOTX48NG}"
+mkdir -p ~/"${DOTX48NG}"
 
-[ -d ~/${DOTX48NG} ] && rm -fr ~/${DOTX48NG}
-mkdir -p ~/${DOTX48NG}
+[ -e "${CONFIG_FILE}" ] && mv "${CONFIG_FILE}" "${CONFIG_FILE}".orig
+x48ng --print-config > "${CONFIG_FILE}"
 
-x48ng --print-config > ~/${DOTX48NG}/config.lua
+cp -r @PREFIX@/share/x48ng/ROMs/ ~/"${DOTX48NG}"/
 
-cp @PREFIX@/share/x48ng/ROMs/$ROM ~/${DOTX48NG}/rom
-cd ~/${DOTX48NG}
+cd ~/"${DOTX48NG}"/ROMs/ || exit 1
+make get-roms
+
+cd ~/"${DOTX48NG}" || exit 1
+[ -e rom ] && mv rom rom.orig
+cp "$ROM" rom
 
 PORT1_SIZE=128K
 PORT2_SIZE=4M
 
-if $(echo $ROM | grep -q "^sx"); then
+if $(echo "$ROM" | grep -q "^sx"); then
     PORT2_SIZE=128K
 fi
 
+[ -e port1 ] && mv port1 port1.orig
 @PREFIX@/share/x48ng/mkcard $PORT1_SIZE port1
+
+[ -e port2 ] && mv port2 port2.orig
 @PREFIX@/share/x48ng/mkcard $PORT2_SIZE port2
