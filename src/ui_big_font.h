c9785a2315d0bd633bf6ab342eacaf44bb696fe9
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2024-09-10 13:00:48 +0200

    (re-)externalize bitmaps in their own header files

---
+++ src/ui_big_font.h
@@ -0,0 +1,172 @@
+#ifndef _UI_BIG_FONT_H
+#define _UI_BIG_FONT_H 1
+
+#define big_font_dot_width 8
+#define big_font_dot_height 13
+static unsigned char big_font_dot_bitmap[] = {
+    0, 0, 0, 0, 0, 0, 0, 24, 24, 0, 0, 0, 0,
+};
+
+#define big_font_0_width 8
+#define big_font_0_height 13
+static unsigned char big_font_0_bitmap[] = {
+    24, 60, 102, 102, 195, 195, 195, 195, 195, 102, 102, 60, 24,
+};
+
+#define big_font_1_width 8
+#define big_font_1_height 13
+static unsigned char big_font_1_bitmap[] = {
+    24, 28, 30, 27, 24, 24, 24, 24, 24, 24, 24, 24, 255,
+};
+
+#define big_font_2_width 8
+#define big_font_2_height 13
+static unsigned char big_font_2_bitmap[] = {
+    60, 102, 195, 195, 192, 192, 96, 56, 12, 6, 3, 3, 255,
+};
+
+#define big_font_3_width 8
+#define big_font_3_height 13
+static unsigned char big_font_3_bitmap[] = {
+    60, 102, 195, 195, 192, 96, 56, 96, 192, 195, 195, 102, 60,
+};
+
+#define big_font_4_width 8
+#define big_font_4_height 13
+static unsigned char big_font_4_bitmap[] = {
+    96, 112, 120, 108, 102, 99, 99, 255, 96, 96, 96, 96, 96,
+};
+
+#define big_font_5_width 8
+#define big_font_5_height 13
+static unsigned char big_font_5_bitmap[] = {
+    255, 3, 3, 3, 3, 59, 103, 192, 192, 192, 195, 102, 60,
+};
+
+#define big_font_6_width 8
+#define big_font_6_height 13
+static unsigned char big_font_6_bitmap[] = {
+    60, 102, 67, 3, 3, 59, 103, 195, 195, 195, 195, 102, 60,
+};
+
+#define big_font_7_width 8
+#define big_font_7_height 13
+static unsigned char big_font_7_bitmap[] = {
+    255, 192, 192, 96, 96, 48, 48, 24, 24, 12, 12, 6, 6,
+};
+
+#define big_font_8_width 8
+#define big_font_8_height 13
+static unsigned char big_font_8_bitmap[] = {
+    60, 102, 195, 195, 195, 102, 60, 102, 195, 195, 195, 102, 60,
+};
+
+#define big_font_9_width 8
+#define big_font_9_height 13
+static unsigned char big_font_9_bitmap[] = {
+    62, 195, 195, 195, 195, 195, 230, 220, 192, 192, 194, 102, 60,
+};
+
+#define big_font_A_width 8
+#define big_font_A_height 10
+static unsigned char big_font_A_bitmap[] = {
+    30, 51, 51, 51, 51, 51, 63, 51, 51, 51,
+};
+
+#define big_font_C_width 8
+#define big_font_C_height 10
+static unsigned char big_font_C_bitmap[] = {
+    30, 51, 51, 3, 3, 3, 3, 51, 51, 30,
+};
+
+#define big_font_D_width 8
+#define big_font_D_height 10
+static unsigned char big_font_D_bitmap[] = {
+    15, 27, 51, 51, 51, 51, 51, 51, 27, 15,
+};
+
+#define big_font_E_width 8
+#define big_font_E_height 10
+static unsigned char big_font_E_bitmap[] = {
+    63, 3, 3, 3, 31, 3, 3, 3, 3, 63,
+};
+
+#define big_font_G_width 8
+#define big_font_G_height 10
+static unsigned char big_font_G_bitmap[] = {
+    30, 51, 51, 3, 3, 59, 51, 51, 51, 30,
+};
+
+#define big_font_H_width 8
+#define big_font_H_height 10
+static unsigned char big_font_H_bitmap[] = {
+    51, 51, 51, 51, 63, 51, 51, 51, 51, 51,
+};
+
+#define big_font_I_width 8
+#define big_font_I_height 10
+static unsigned char big_font_I_bitmap[] = {
+    63, 12, 12, 12, 12, 12, 12, 12, 12, 63,
+};
+
+#define big_font_L_width 8
+#define big_font_L_height 10
+static unsigned char big_font_L_bitmap[] = {
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 63,
+};
+
+#define big_font_M_width 8
+#define big_font_M_height 10
+static unsigned char big_font_M_bitmap[] = {
+    33, 51, 51, 63, 63, 51, 51, 51, 51, 51,
+};
+
+#define big_font_N_width 8
+#define big_font_N_height 10
+static unsigned char big_font_N_bitmap[] = {
+    51, 51, 55, 55, 55, 59, 59, 59, 51, 51,
+};
+
+#define big_font_O_width 8
+#define big_font_O_height 10
+static unsigned char big_font_O_bitmap[] = {
+    30, 51, 51, 51, 51, 51, 51, 51, 51, 30,
+};
+
+#define big_font_P_width 8
+#define big_font_P_height 10
+static unsigned char big_font_P_bitmap[] = {
+    31, 51, 51, 51, 51, 31, 3, 3, 3, 3,
+};
+
+#define big_font_R_width 8
+#define big_font_R_height 10
+static unsigned char big_font_R_bitmap[] = {
+    31, 51, 51, 51, 31, 27, 51, 51, 51, 51,
+};
+
+#define big_font_S_width 8
+#define big_font_S_height 10
+static unsigned char big_font_S_bitmap[] = {
+    30, 51, 51, 6, 12, 12, 24, 51, 51, 30,
+};
+
+#define big_font_T_width 8
+#define big_font_T_height 10
+static unsigned char big_font_T_bitmap[] = {
+    63, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+};
+
+#define big_font_V_width 8
+#define big_font_V_height 10
+static unsigned char big_font_V_bitmap[] = {
+    51, 51, 51, 51, 51, 51, 51, 30, 30, 12,
+};
+
+#define big_font_X_width 8
+#define big_font_X_height 10
+static unsigned char big_font_X_bitmap[] = {
+    51, 51, 30, 30, 12, 12, 30, 30, 51, 51,
+};
+
+#endif /* _UI_BIG_FONT_H 1 */
